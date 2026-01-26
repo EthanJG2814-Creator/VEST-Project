c414a49c96e82f2037123d824f963bb3d93aa3df
Person <s.man6917@gmail.com>
2026-01-26 08:51:24 -0600

    Hopefully removed the .vs folder from being commited.

--- VEST/obj/Debug/net9.0/VEST.AssemblyInfo.cs
+++ VEST/obj/Debug/net9.0/VEST.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f402ed4fc1d2a3c7598c0442f717482399cd71c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cef6ded17f238f50fa7274559a599e944f6438ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("VEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
