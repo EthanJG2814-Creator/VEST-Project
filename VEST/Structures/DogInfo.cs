a7a350f6997a7182391c21583bc0a5ee2c297479
Person <s.man6917@gmail.com>
2026-01-26 10:26:15 -0600

    Added 2 interfaces and 2 structures Interfaces: - APIMessenger: This is the start of the main api communicator, with the core methods abstractly added. Public methods are still missing as I do not know what api calls will be needed. - ICompressable: This indicates a class is compressable. The 2 methods needed are already added and this interface is complete.
    Structures:
    - DataPoint: A point in time that the vest measured the dog's metrics. This only has a few basic data points and will need more variables added to it based on what the vest actually measures.
    - DogInfo: Basic information about a dog such as  name breed, birthdate, and so on. I based this off of last semester's info page, so may still need some tweaking.

---
+++ VEST/Structures/DogInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace VEST.Structures
+{
+    /// <summary>
+    /// The basic information about a dog.
+    /// </summary>
+    public readonly struct DogInfo(string name, string breed, DateTime birthDate, float weightKg, long microchipId)
+    {
+        public readonly string Name = name;
+        public readonly string Breed = breed;
+        public readonly DateTime BirthDate = birthDate;
+        public readonly float WeightKg = weightKg;
+        public readonly long MicrochipId = microchipId;
+
+        public readonly TimeSpan Age => DateTime.Now - BirthDate;
+    }
+}
