9c5ce9ad17c268ee965be4883277e7f214716929
Person <s.man6917@gmail.com>
2026-02-07 22:29:41 -0600

    Added simple endpoint for posting a packet for a certain dog.

--- VEST/API/VestAPICaller.cs
+++ VEST/API/VestAPICaller.cs
@@ -1,4 +1,5 @@
-﻿using VEST.Models.Abstract_Classes;
+﻿using Newtonsoft.Json.Linq;
+using VEST.Models.Abstract_Classes;
 
 namespace VEST.API
 {
@@ -25,9 +26,19 @@ namespace VEST.API
         /// available; otherwise, null.</returns>
         public static async Task<(bool success, HttpResponseMessage? response)> CallExampleEndpoint()
         {
-            HttpRequestMessage request = new(HttpMethod.Get, $"{MAIN_API_URL}readings");
+            //?dog_id=58dd8ca7-9bd9-4de3-a671-7dc5a9bf5f4&device_id=49546760-2d35-41f7-8af0-faccaf3b4d5c&type_key=ppg_adc
+            HttpRequestMessage request = new(HttpMethod.Get, $"{MAIN_API_URL}");
             return await SendInfo(request, Client);
         }
 
+        public static async Task<(bool success, JToken? response)> SendReadings(string dogId, string deviceId, string typeKey, JToken data)
+        {
+            HttpRequestMessage request = new(HttpMethod.Post, $"{MAIN_API_URL}readings?dog_id={dogId}&device_id={deviceId}&type_key={typeKey}")
+            {
+                Content = new StringContent(data.ToString(), System.Text.Encoding.UTF8, "application/json")
+            };
+            var (success, response) = await SendInfo(request, Client);
+            return (success, success ? JToken.Parse(await response!.Content.ReadAsStringAsync()) : null);
+        }
     }
 }
